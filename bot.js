eaeb6cef1bb94653fa41b6f76ffa6f95c9368d20
TheTarr <1491362843@qq.com>
2022-04-22 22:37:01 +0400

    聊天

--- bot.js
+++ bot.js
@@ -187,6 +187,87 @@ listener.on('message', msg => {
                     toot(`@${acct} ` + reply + " 操！", id, visib);
                 });
             }
+
+            // 道歉
+            const regex9 = /(道歉|不孝女|你错了|可知罪|孽畜|小畜牲)/i;
+            if (regex9.test(content)) {
+                console.log("somebody ask for a apologize");
+                const acct = msg.data.account.acct;
+                const reply = `@${acct} 对不起！！！操！`;
+                toot(reply, id, visib);
+            }
+
+            // 问好
+            const regex10 = /(你好|在吗)/i;
+            if (regex10.test(content)) {
+                console.log("somebody ask for a hi");
+                const acct = msg.data.account.acct;
+                const reply = `@${acct} 你好！我在！操！`;
+                toot(reply, id, visib);
+            }
+
+            // // 什么事这么激动
+            // const regex11 = /(啊啊啊|杀杀杀)/i;
+            // if (regex11.test(content)) {
+            //     console.log("i will interact");
+            //     const acct = msg.data.account.acct;
+            //     const reply = `@${acct} 什么事这么激动？操！`;
+            //     toot(reply, id, visib);
+            // }
+
+            // 机器人
+            const regex12 = /(是机器人|是活人|是真人)/i;
+            if (regex12.test(content)) {
+                console.log("somebody ask for a answer");
+                const acct = msg.data.account.acct;
+                const reply = `@${acct} 操操当然是机器人！操！`;
+                toot(reply, id, visib);
+            }
+
+            // 骂人
+            const regex13 = /(我是你妈|你是狗|你傻逼|骂我|踩我|操操笨蛋|操操傻)/i;
+            if (regex13.test(content)) {
+                console.log("somebody ask for abuse");
+                const acct = msg.data.account.acct;
+                const reply = `@${acct} 你是狗吧！操！`;
+                toot(reply, id, visib);
+            }
+
+            // // 谢谢
+            // const regex14 = /(太牛了|懂事|可爱)/i;
+            // if (regex14.test(content)) {
+            //     console.log("somebody ask for a thx");
+            //     const acct = msg.data.account.acct;
+            //     const reply = `@${acct} 谢谢妞！操！`;
+            //     toot(reply, id, visib);
+            // }
+
+            // 笑
+            const regex15 = /(哈哈哈|笑死)/i;
+            if (regex15.test(content)) {
+                console.log("somebody ask for a haha");
+                const acct = msg.data.account.acct;
+                const reply = `@${acct} 哈哈！操！`;
+                toot(reply, id, visib);
+            }
+
+            // 操
+            const regex16 = /(操！|操啊|我操|操了)/i;
+            if (regex16.test(content)) {
+                console.log("somebody ask for fuck");
+                const acct = msg.data.account.acct;
+                const reply = `@${acct} 操！`;
+                toot(reply, id, visib);
+            }
+
+            // 笑
+            const regex17 = /(嘛呢|在干嘛|干啥呢)/i;
+            if (regex17.test(content)) {
+                console.log("somebody ask for jinkuang");
+                const acct = msg.data.account.acct;
+                const reply = `@${acct} 报告，操操现在无事可做，感觉很无聊！操！`;
+                toot(reply, id, visib);
+            }
         }
     }
 });
