5409b9eef2ee9f645b134d22ce9be704743dcb9a
TheTarr <1491362843@qq.com>
2022-04-28 13:49:51 +0400

    更新文案

--- random_fanfic.py
+++ random_fanfic.py
@@ -6,7 +6,8 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 
 num = random.randint(1,4999)
-host = 'ao3-cn.top'
+host = 'archiveofourown.org'
+in_wall_host = 'login.ao3.site'
 
 try: 
     url = "https://" + host + "/works/search?commit=Search&page=" + str(num) +  "&work_search%5Bbookmarks_count%5D=&work_search%5Bcharacter_names%5D=&work_search%5Bcomments_count%5D=&work_search%5Bcomplete%5D=&work_search%5Bcreators%5D=&work_search%5Bcrossover%5D=&work_search%5Bfandom_names%5D=&work_search%5Bfreeform_names%5D=&work_search%5Bhits%5D=&work_search%5Bkudos_count%5D=&work_search%5Blanguage_id%5D=zh&work_search%5Bquery%5D=&work_search%5Brating_ids%5D=&work_search%5Brelationship_names%5D=&work_search%5Brevised_at%5D=&work_search%5Bsingle_chapter%5D=0&work_search%5Bsort_column%5D=_score&work_search%5Bsort_direction%5D=desc&work_search%5Btitle%5D=&work_search%5Bword_count%5D="
@@ -21,6 +22,6 @@ try:
     num = random.randint(2,len(all_li)-5)
     my_a = all_li[num].a
 
-    print("泥嚎！这是操操在 ao3 共计 "+ num_of_article.get_text()[:-9] + " 篇中文作品中专门为你找到的：" + my_a.get_text() + " https://" + host + my_a['href'])
+    print("泥嚎！这是操操在 ao3 共计 "+ num_of_article.get_text()[:-9] + " 篇中文作品中专门为你找到的：" + my_a.get_text() + " https://" + in_wall_host + my_a['href'])
 except:
     print("目前与 " + host + " 之间通信出现问题！抱歉！")
